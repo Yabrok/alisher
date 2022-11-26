7a08a2e6671e346d470e55012d0910916c7e82b6
Yabrok <nosor321@mail.ru>
2022-11-27 03:42:42 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var tickets = 500;
+var hotel = 250;
+var rest = 120;
+var travel = tickets + hotel  * 11225 + rest * 11686;
+
+
+var bank = +prompt('Budjetingiz? (sum):');
+
+var balance = bank - travel;
+
+var eltitle = document.querySelector('.title');
+
+if (balance >= 0) {
+    eltitle.textContent = 'Oq yul Alisher';
+}else {
+    eltitle.textContent = 'Pul yoq Alisher';
+}
+
